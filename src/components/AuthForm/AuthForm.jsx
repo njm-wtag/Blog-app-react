ce01e8c5de593dbe32807461edba720f30901e04
Nusrat Jahan Mitu <148762372+njm-wtag@users.noreply.github.com>
2024-01-23 18:53:37 +0600

    resolved review of auth form validation. removed comments, modified authUtils

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Field, Form } from "react-final-form";
 import { Link, useLocation } from "react-router-dom";
 import "./AuthForm.scss";
+import authValidation from "../../utils/authValidation";
 const AuthForm = ({ handleSubmit, responseMessage }) => {
   const location = useLocation();
   const pathname = location.pathname;
@@ -19,35 +20,7 @@ const AuthForm = ({ handleSubmit, responseMessage }) => {
       </div>
       <Form
         onSubmit={handleSubmit}
-        validate={(values) => {
-          const errors = {};
-          if (isRegisterForm) {
-            if (!values.firstname) {
-              errors.firstname = "First name is required";
-            }
-            if (!values.lastname) {
-              errors.lastname = "Last name is required";
-            }
-            if (!values.email) {
-              errors.email = "Email is required";
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = "Invalid email address";
-            }
-          }
-
-          if (!values.username) {
-            errors.username = "User name is required";
-          }
-
-          if (!values.password) {
-            errors.password = "Password is required";
-          } else if (values.password.length < 8) {
-            errors.password = "Password must be at least 8 characters";
-          }
-          return errors;
-        }}
+        validate={(values) => authValidation(values, isRegisterForm)}
         render={({ submitError, handleSubmit }) => (
           <form className="form-container__form" onSubmit={handleSubmit}>
             {isRegisterForm && (
