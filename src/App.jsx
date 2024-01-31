6948a0084243fd327cd497c14d2260f1374d425a
Nusrat Jahan Mitu <148762372+njm-wtag@users.noreply.github.com>
2024-01-31 16:25:53 +0600

    merged task 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import Home from "./Pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Profile from "./Pages/Profile";
-import Header from "./components/Header/Header";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import { useSelector } from "react-redux";
 
 function App() {
+  const { authUser } = useSelector((state) => state.auth);
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/" element={<PrivateRoute />}>
-          <Route path={authUser.username} element={<Profile />} />
+          <Route path={authUser?.username} element={<Profile />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const { authUser } = useSelector((state) => state.auth);
   return (
     <Layout>
       <h1> Home </h1>
-      <Link to="/profile">Profile</Link>
+      {authUser && <Link to={`/${authUser?.username}`}>Profile</Link>}
     </Layout>
   );
 };
