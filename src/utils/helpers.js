3d4431864a2a14ae197dd55d6e4399747ef13c26
Nusrat Jahan Mitu <148762372+njm-wtag@users.noreply.github.com>
2024-02-14 14:46:05 +0600

    Refactor: added helper file

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,13 +1,3 @@
-import { mockAuthors, mockCategories } from "../data/mockData";
-
-export const getCategoryById = (categoryId) => {
-  return mockCategories?.find((category) => category.id === categoryId);
-};
-
-export const getAuthorById = (authorId) => {
-  return mockAuthors?.find((author) => author.id === authorId);
-};
-
 export const convertToBase64 = (file) => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
